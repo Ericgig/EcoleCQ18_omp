72943fc7fe20cba9e29e804237cf7c4af20b0ffb
Eric <eric.giguere@calculquebec.ca>
2018-05-14 10:27:23 -0400

    pi2.2.c

--- pi2.2.c
+++ pi2.2.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <omp.h>
-#define NUM_THREADS 8
+#define MAX_NUM_THREADS 40
 
 int main(void) {
-int i, N=1000000000, fil_n;
-double Sum =0., sum[NUM_THREADS];
-omp_set_num_threads(NUM_THREADS);
+int i, N=1000000000, fil_n, num_threads;
+double Sum =0., sum[MAX_NUM_THREADS];
 
-#pragma omp parallel default(none) shared(sum,N) private(fil_n,i)
+#pragma omp parallel private(fil_n)
   {
     fil_n = omp_get_thread_num();
+    num_threads = omp_get_num_threads();
     sum[fil_n] = 0.;
 #pragma omp for
     for(i=0;i<N;i+=2){
-  sum[fil_n] += 4./(2.*i+1.);
-  sum[fil_n] -= 4./(2.*i+3.);
+      sum[fil_n] += 4./(2.*i+1.);
+      sum[fil_n] -= 4./(2.*i+3.);
     }
   }
-  for(i=0;i<NUM_THREADS;i++){
-Sum += sum[i];
+  for(i=0;i<num_threads;i++){
+    Sum += sum[i];
   }
   printf("pi = %f\n",Sum);
 }

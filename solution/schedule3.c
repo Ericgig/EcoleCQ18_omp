bd73d80589e31ddd5d8b0ac41ef5907ce635d618
ericgig ericgig <eric.giguere@calculquebec.ca>
2021-05-24 17:59:45 +0000

    Prepare for 2021

---
+++ solution/schedule3.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <omp.h>
+#include <unistd.h>
+
+#define N 100
+
+int main(void) {
+int i;
+printf("schedule(static, 2)\n");
+#pragma omp parallel
+  {
+    char done_here[N+1];
+    done_here[N] = 0;
+    for(i=0; i<N; i++){done_here[i] = '.';}
+#pragma omp for schedule(static, 2)
+    for(i=0; i<N; i++){
+      usleep(1000*((i*i)%10));
+      done_here[i] = 'o';
+    }
+  printf("Fil %d: %s \n", omp_get_thread_num(), done_here);
+  }
+
+
+printf("schedule(dynamic, 2)\n");
+#pragma omp parallel
+  {
+    char done_here[N+1];
+    done_here[N] = 0;
+    for(i=0; i<N; i++){done_here[i] = '.';}
+#pragma omp for schedule(dynamic, 2)
+    for(i=0; i<N; i++){
+      usleep(1000*((i*i)%10));
+      done_here[i] = 'o';
+    }
+  printf("Fil %d: %s \n", omp_get_thread_num(), done_here);
+  }
+
+printf("schedule(guided, 2)\n");
+#pragma omp parallel
+  {
+    char done_here[N+1];
+    done_here[N] = 0;
+    for(i=0; i<N; i++){done_here[i] = '.';}
+#pragma omp for schedule(guided, 2)
+    for(i=0; i<N; i++){
+      usleep(1000*((i*i)%10));
+      done_here[i] = 'o';
+    }
+  printf("Fil %d: %s \n", omp_get_thread_num(), done_here);
+  }
+}
